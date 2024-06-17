e7ae53936f1a43a77ac39528b2a8007191f06138
Han Jia <hanj@datacom.co.nz>
2024-06-17 14:44:43 +1200

    add select control

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import { useAppState } from "../hooks/useAppState";
+import Select from "react-select";
+import { groupedOptions } from "../assets/data";
 
 export const Page = () => {
   const { appData, liveData } = useAppState();
   const [, setFlag] = useState(Date.now().toString());
 
   console.log("live data", liveData);
+
+  const onChangeHandler = (val: unknown) => {
+    console.log(val, "val");
+  }
 
   return (
     <>
@@ -18,6 +24,12 @@ export const Page = () => {
       >
         Click
       </button>
+
+      <Select
+        options={groupedOptions}
+        onChange={onChangeHandler}
+        isMulti={false}
+      />
     </>
   );
 };
