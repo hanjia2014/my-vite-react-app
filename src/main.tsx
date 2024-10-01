97827e76ebe623c91c140580bdc96a8b6b0dee2b
Han Jia <hanj@datacom.co.nz>
2024-10-02 10:19:33 +1300

    add  primeflex

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { AppStateProvider } from "./providers/state.provider.tsx";
 import { BrowserRouter } from "react-router-dom";
+import "primeflex/primeflex.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
